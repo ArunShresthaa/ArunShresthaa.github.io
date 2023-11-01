f23af2e878996af772c021527ff62873df128618
Arun Shrestha <arunstha547@gmail.com>
2023-11-01 14:40:23 +0545

    uploading src

---
+++ src/constants/index.js
@@ -0,0 +1,108 @@
+import {
+  frontend,
+  backend,
+  ux,
+  prototyping,
+  javascript,
+  typescript,
+  html,
+  css,
+  reactjs,
+  redux,
+  tailwind,
+  nodejs,
+  git,
+  figma,
+  docker,
+  postgresql,
+  rubyrails,
+  graphql,
+  komikult,
+  leaderboard,
+  math,
+  movie,
+  nyeusi,
+  space,
+  coverhunt,
+  dcc,
+  kelhel,
+  microverse,
+} from '../assets';
+
+export const navLinks = [
+  {
+    id: 'about',
+    title: 'About',
+  },
+  {
+    id: 'projects',
+    title: 'Projects',
+  },
+  {
+    id: 'contact',
+    title: 'Contact',
+  },
+];
+
+const services = [
+  {
+    title: 'Frontend Developer',
+    icon: frontend,
+  },
+];
+
+const technologies = [
+  {
+    name: 'HTML',
+    icon: html,
+  },
+  {
+    name: 'CSS',
+    icon: css,
+  },
+  {
+    name: 'JavaScript',
+    icon: javascript,
+  },
+  {
+    name: 'git',
+    icon: git,
+  },
+];
+
+const experiences = [
+  {
+    title: 'Front-End Developer',
+    company_name: '',
+    icon: coverhunt,
+    iconBg: '#333333',
+    date: 'Aug 2021 - Feb 2022',
+  },
+];
+
+const projects = [
+  {
+    id: 'project-1',
+    name: 'KomiKult',
+    description: 'A comic characters list app that displays Marvel characters.',
+    tags: [
+      {
+        name: 'react',
+        color: 'blue-text-gradient',
+      },
+      {
+        name: 'mongodb',
+        color: 'green-text-gradient',
+      },
+      {
+        name: 'tailwind',
+        color: 'pink-text-gradient',
+      },
+    ],
+    image: komikult,
+    repo: 'https://github.com/shaqdeff/KomiKult',
+    demo: 'https://shaqdeff.github.io/KomiKult/',
+  },
+];
+
+export { services, technologies, experiences, projects };
